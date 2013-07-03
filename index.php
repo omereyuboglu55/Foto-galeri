3bab746082220e4d92e2bb2c49cc9b1e4b7fc586
Samet ATABAŞ <sametatabasch@gmail.com>
2013-07-03 23:03:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Şeyma ÖZDEMİR</title>
+		<title>Foto Galeri</title>
 		<link rel="shortcut icon" href="../favicon.ico">
 		<meta name="description" content="Açıklama" />
         <meta name="keywords" content="" />
